dc56b48cd34684f7a02f313297297d2102d91c7c
NTILinea <linea.petersen@elev.ga.ntig.se>
2021-03-02 13:19:10 +0100

    burger menu index
    added all the other pages to the site that's needed. started working on the burger menu on the index file, had a bit of trouble but was fixed. gonna change to the look of it more

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navLinks')
+const menu = document.querySelector('.menu')
+
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    menu.classList.toggle('toggle')
+  
+}
+menu.addEventListener('click', toggleMeny)
